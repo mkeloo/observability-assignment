f1273a8cd5f850e2a37a2a68c31bc7df27fd84cf
mkeloo <mkeloo@ufl.edu>
2023-11-22 06:37:23 -0500

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const tracer = require('./tracing')('todo-service');
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const { StatusCode } = require('@opentelemetry/api');
 
 const app = express();
 app.use(express.json());
@@ -8,26 +9,50 @@ const port = 3000;
 let db;
 
 const startServer = async () => {
-  const client = await MongoClient.connect('mongodb://localhost:27017/');
-  db = client.db('todo');
-  await db.collection('todos').insertMany([
-    { id: '1', title: 'Buy groceries' },
-    { id: '2', title: 'Install Aspecto' },
-    { id: '3', title: 'buy my own name domain' },
-  ]);
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
+  try {
+    const client = await MongoClient.connect('mongodb://localhost:27017/');
+    db = client.db('todo');
+    await db.collection('todos').insertMany([
+      { id: '1', title: 'Buy groceries' },
+      { id: '2', title: 'Install Aspecto' },
+      { id: '3', title: 'Buy my own name domain' },
+    ]);
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.error('Failed to connect to MongoDB:', error);
+  }
 };
 
 startServer();
 
 app.get('/todo', async (req, res) => {
-  const todos = await db.collection('todos').find({}).toArray();
-  res.send(todos);
+  const span = tracer.startSpan('Get Todos');
+  try {
+    const todos = await db.collection('todos').find({}).toArray();
+    res.send(todos);
+  } catch (error) {
+    span.setStatus({ code: StatusCode.ERROR, message: error.message });
+    res.status(500).send('error fetching todos');
+  } finally {
+    span.end();
+  }
 });
 
 app.get('/todo/:id', async (req, res) => {
-  const todo = await db.collection('todos').findOne({ id: req.params.id });
-  res.send(todo);
+  const span = tracer.startSpan('Get Todo By ID');
+  try {
+    const todo = await db.collection('todos').findOne({ id: req.params.id });
+    if (!todo) {
+      res.status(404).send('todos not found');
+    } else {
+      res.send(todo);
+    }
+  } catch (error) {
+    span.setStatus({ code: StatusCode.ERROR, message: error.message });
+    res.status(500).send('errror fetching todo');
+  } finally {
+    span.end();
+  }
 });
--- tracing.js
+++ tracing.js
@@ -2,26 +2,26 @@ const { Resource } = require('@opentelemetry/resources');
 const {
   SemanticResourceAttributes,
 } = require('@opentelemetry/semantic-conventions');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
 const { SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
 const { trace } = require('@opentelemetry/api');
 
-//Instrumentations
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
+
 const {
   ExpressInstrumentation,
 } = require('opentelemetry-instrumentation-express');
-
 const {
   MongoDBInstrumentation,
 } = require('@opentelemetry/instrumentation-mongodb');
-
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 
-//Exporter
 module.exports = (serviceName) => {
-  const exporter = new ConsoleSpanExporter();
+  const exporter = new JaegerExporter({
+    endpoint: 'http://localhost:14268/api/traces',
+  });
+
   const provider = new NodeTracerProvider({
     resource: new Resource({
       [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
@@ -39,5 +39,6 @@ module.exports = (serviceName) => {
     ],
     tracerProvider: provider,
   });
+
   return trace.getTracer(serviceName);
 };
